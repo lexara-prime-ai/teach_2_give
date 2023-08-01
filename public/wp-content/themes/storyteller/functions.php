2da7b80589145e8ee1e912e90f654adb3fc2bffd
Irfan Ghat <irfanghat@gmail.com>
2023-08-01 13:32:29 +0300

    Updated stylesheet and functions.php

--- public/wp-content/themes/storyteller/functions.php
+++ public/wp-content/themes/storyteller/functions.php
@@ -6,7 +6,7 @@ function story_teller_theme_files()
     wp_enqueue_script('story_teller_main_script', get_theme_file_uri('/js/main.js'), null, microtime(), true);
     wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap');
     wp_enqueue_style('font-awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css');
-    wp_enqueue_style('story_teller_main_styles', get_stylesheet_uri(), null, microtime());
+    wp_enqueue_style('story_teller_main_styles', get_stylesheet_uri('/style.css'), null, microtime());
 }
 
 add_action('wp_enqueue_scripts', 'story_teller_theme_files');
